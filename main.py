8d08fe09c800080bcb0721f50b7c0444cf91fe9b
klforthwind <kl.forthwind@gmail.com>
2020-09-23 00:58:18 +0000

    add a loop to check streamers live

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import discord
 from discord.ext import commands
+from twitchapi import is_live
 
 bot = commands.Bot(command_prefix='')
+
+streamers = list()
+live = []
+
+async def check_live():
+    await client.wait_until_ready() # ensures cache is loaded
+    while not client.is_closed():
+        for strim_num in range(len(streamers)):
+            strim = streamers[strim_num]
+            strimer = strim[0]
+            streaming = is_live(strimer)
+            if streaming:
+                if strimer not in live:
+                    live.append(strimer)
+                    await strim[1].send(f"{} is live!"%strimer)
+            elif strimer in live:
+                live.remove(strimer)
+
+        await asyncio.sleep(60)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {} with id {}'%(client.user.name, str(client.user.id)))
+    client.loop.create_task(check_live())
 
 @bot.event
 async def on_message(message):
@@ -12,5 +37,10 @@ async def on_message(message):
         await message.channel.send('🐸')
     if 'frog' in message.content.lower():
         await message.add_reaction('🐸')
+
+    if 'setStreamer' in message:
+        if len(message.split()) == 2 and message.member.hasPermission("MUTE_MEMBERS"):
+            streamers.append([message.split()[1], message.channel])
+            print(streamers)
 
 bot.run('token redacted')
